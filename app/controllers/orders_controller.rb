3527d13bddfab836e9790a81fff543218f55f77a
ryotanakatsuka <71533935+ryotanakatsuka@users.noreply.github.com>
2020-11-07 00:29:18 +0900

    商品購入機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,7 +7,9 @@ class OrdersController < ApplicationController
   
   def create
     @order=Order.new(order_params)
+    @item=Item.find(params[:item_id])
     if @order.valid?
+      pay_item
       @order.save
       redirect_to root_path
     else
@@ -18,6 +20,15 @@ class OrdersController < ApplicationController
 
   private
   def order_params
-    params.require(:order).permit(:user_id,:item_id).merge(user_id: current_user.id,item_id: params[:item_id])
+    params.permit(:user_id,:item_id).merge(user_id: current_user.id,item_id: params[:item_id],token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] 
+      Payjp::Charge.create(
+        amount: @item.price,  
+        card: order_params[:token],    
+        currency: 'jpy'                
+      )
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :item
-  
+  attr_accessor :token
+  validates :token, presence: true
 end
