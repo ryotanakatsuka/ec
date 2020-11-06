2a90f3e608dbcaae5b717896bc2ba1f41f37e75c
ryotanakatsuka <71533935+ryotanakatsuka@users.noreply.github.com>
2020-11-06 22:11:30 +0900

    購入記録の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,23 @@
 class OrdersController < ApplicationController
+
   def new
     @item=Item.find(params[:item_id])
+    @order=Order.new
+  end
+  
+  def create
+    @order=Order.new(order_params)
+    if @order.valid?
+      @order.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
-  def create
-    
+
+  private
+  def order_params
+    params.require(:order).permit(:user_id,:item_id).merge(user_id: current_user.id,item_id: params[:item_id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -61,8 +61,12 @@ ActiveRecord::Schema.define(version: 2020_11_05_122643) do
   end
 
   create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "item_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_orders_on_item_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -84,4 +88,6 @@ ActiveRecord::Schema.define(version: 2020_11_05_122643) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "addresses", "users"
   add_foreign_key "items", "users"
+  add_foreign_key "orders", "items"
+  add_foreign_key "orders", "users"
 end
