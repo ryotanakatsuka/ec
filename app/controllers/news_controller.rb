ac0aa3c3866b2b2dbe3eeae878d14ec13222da52
ryotanakatsuka <71533935+ryotanakatsuka@users.noreply.github.com>
2020-11-17 17:06:40 +0900

    投稿データの保存の実装

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,10 +1,27 @@
 class NewsController < ApplicationController
   
   def index
-    @news=New.all
+    
   end
 
   def new
     @new=New.new
+    
+  end
+  
+  def create
+    @new=New.create(new_params)
+    if @new.valid?
+      @new.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def new_params
+    params.require(:new).permit(:text,:image,:title).merge(user_id: current_user.id)
   end
 end
