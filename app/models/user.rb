8526be532eafcbdf8f2fb64b22ff9ff1fb295ad8
ryotanakatsuka <71533935+ryotanakatsuka@users.noreply.github.com>
2020-11-28 16:40:18 +0900

    漢字・カタカナの正規表現実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          with_options presence: true do 
-          validates :first_name         
-          validates :last_name                
-          validates :first_name_kana    
-          validates :last_name_kana     
+          validates :first_name,         format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+          validates :last_name,          format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}                
+          validates :first_name_kana,    format: { with: /\A[ァ-ヶー－]+\z/}
+          validates :last_name_kana,     format: { with: /\A[ァ-ヶー－]+\z/}
           
         end
         has_one :address,dependent: :destroy
