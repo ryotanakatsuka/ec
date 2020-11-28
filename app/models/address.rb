f043992ef782343ce0f0d9025d1685b4dc872a11
ryotanakatsuka <71533935+ryotanakatsuka@users.noreply.github.com>
2020-11-28 17:15:17 +0900

    郵便番号の正規表現

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,7 +4,7 @@ class Address < ApplicationRecord
     belongs_to :prefecture
     
   with_options presence: true do
-    validates :postal_code
+    validates :postal_code,format: { with: /\A\d{3}[-]\d{4}\z/, message: 'を入力してください' }
     validates :prefecture_id
     validates :addresses
     validates :phone_number
